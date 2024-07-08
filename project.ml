089a919cee31030333c6d87912b45209dd640ce7
Giacomo Maldarella <giacomomaldarella@gmail.com>
2024-07-08 16:48:23 +0200

    Finito styling e assert

--- project.ml
+++ project.ml
@@ -14,7 +14,7 @@ type conf_level =
 
 (* 'env' is a list of: identifier ('ide') - value ('v') - taintness ('bool')*) 
 type 'v env = (ide * 'v  * bool) list
-(* FALLO*) 
+(* 'c_env' is a list of: identifier ('ide') - confidential level ('conf_level')*) 
 type 'v c_env = (ide * conf_level) list
 (*'priv_TB' is a list of: identifier ('ide') - security level ('sec_level')*)
 type 'v priv_TB = (ide * sec_level) list
@@ -27,7 +27,7 @@ type exp =
 | Prim of ide * exp * exp
 | If of exp * exp * exp
 | Let of ide * exp * exp
-| LetIn of ide * exp * exp (* to test the taint analysis *)
+| LetIn of ide * exp * exp  (* to test the taint analysis *)
 | Fun of ide * exp
 | Apply of exp * exp
 | Trustblock of exp 
@@ -38,7 +38,7 @@ type exp =
 | Execute of exp
 | CallHandler of exp * exp
 | Assert of exp
-| End
+| End                         (* end of a trust block*)
 
 type evt =
 | Int of int
@@ -54,17 +54,17 @@ type evt =
 
 let rec lookup env x =
 match env with
-| [] -> failwith (x ^ " not found lookup")
+| [] -> failwith (x ^ " not found in lookup")
 | (y, v, _) :: r -> if x = y then v else lookup r x
 
 let rec t_lookup env x =
 match env with
-| [] -> failwith (x ^ " not found t_lookup")
+| [] -> failwith (x ^ " not found in t_lookup")
 | (y, _, t) :: r -> if x = y then t else t_lookup r x
 
 let rec c_lookup env x =
   match env with
-  | [] -> failwith (x ^ " not found c_lookup")
+  | [] -> failwith (x ^ " not found in c_lookup")
   | (y, c) :: r -> if x = y then c else c_lookup r x
 
 let rec secret_lookup priv_TB x = 
@@ -78,7 +78,6 @@ let rec pub_lookup priv_TB x =
   | (y, s) :: r -> if x = y && s = Public then true else pub_lookup r x
 
 let rec handle_lookup priv_TB x = 
-  (* let _ = print_list priv_TB in *)
   match priv_TB, x with
   | [], _ -> false
   | _, Var y -> (
@@ -101,17 +100,6 @@ let bind_cf c_env (x:ide) (cf:conf_level) = (x,cf)::c_env
 let string_of_conf_level = function
   | High -> "High"
   | Low -> "Low"
- let rec print_list priv_TB =
-  (*Just to display on the terminal the evaluation result*)
-  match priv_TB with
-  | [] -> Printf.printf " Lista vuota \n"
-  | (x,s,_) :: rest -> let _ = Printf.printf " Ide %s, " x (*(string_of_conf_level s)*) in 
-                    print_list rest 
-
-(*let rec enhanceConfLevel (x: ide) (c_env: conf_level c_env) =
-  match c_env with
-  | [] -> failwith (x ^ " not found in changeConfLevel")
-  | (y, c) :: r -> if x = y then (y, High) :: r else (y, c) :: enhanceConfLevel x r*)
 
 (*----------------------------------------------------------------------------------------------------*)
 (*INTERPRETER*)
@@ -126,7 +114,7 @@ match e with
   if inTrustBlock  && (secret_lookup priv_TB x) || (pub_lookup priv_TB x) then 
     (lookup env x, t_lookup env x)
   else
-    if secret_lookup priv_TB x then failwith "Can't access a secret"
+    if secret_lookup priv_TB x then failwith "Can't access a secret entity"
     else if handle_lookup priv_TB (Var x) || not (pub_lookup priv_TB x) then (lookup env x, t_lookup env x)
     else failwith "Can't access a variable not trusted"
 | Prim (op, e1, e2) ->
@@ -134,7 +122,6 @@ match e with
     let v1, t1 = eval e1 env t priv_TB inTrustBlock in
     let v2, t2 = eval e2 env t priv_TB inTrustBlock in
     match (op, v1, v2) with
-    (* taintness of binary ops is given by the OR of the taintness of the args *) 
     | "*", Int i1, Int i2 -> (Int (i1 * i2), t1 || t2)
     | "+", Int i1, Int i2 -> (Int (i1 + i2), t1 || t2)
     | "-", Int i1, Int i2 -> (Int (i1 - i2), t1 || t2)
@@ -151,64 +138,64 @@ match e with
     | Bool false -> let v3, t3 = eval e3 env t priv_TB inTrustBlock in (v3, t1 || t3) 
     | _ -> failwith "Unexpected condition."
   end
-| Let (x, exprRight, letBody) ->
-    if inTrustBlock then failwith "You can't declare a variable in a TrustBlock" 
+| Let (x, value, body) ->
+    if inTrustBlock then failwith "You can't use Let in a TrustBlock" 
     else
-      let xVal, taintness = eval exprRight env t priv_TB inTrustBlock in
-      let letEnv = bind_env env x xVal taintness in
-      eval letBody letEnv t priv_TB inTrustBlock
-| LetIn (x, exprRight, letBody) -> 
+      let v, taintness = eval value env t priv_TB inTrustBlock in
+      let newEnv = bind_env env x v taintness in
+      eval body newEnv t priv_TB inTrustBlock
+| LetIn (x, value, body) -> 
     if (inTrustBlock) then failwith "You can't take a var from input in a TrustBlock"
     else
-    let xVal, taintness = eval exprRight env true priv_TB inTrustBlock in
-    let letEnv = bind_env env x xVal taintness in
-    eval letBody letEnv true priv_TB inTrustBlock
-| Fun (f_param, fBody) ->
-  (Closure (f_param, fBody, env, priv_TB), t)
+    let v, taintness = eval value env true priv_TB inTrustBlock in
+    let newEnv = bind_env env x v taintness in
+    eval body newEnv true priv_TB inTrustBlock
+| Fun (param, body) ->
+  (Closure (param, body, env, priv_TB), t)
 | Apply (eFun, eArg) -> ( 
   let fClosure, tClosure = eval eFun env t priv_TB inTrustBlock in
   match fClosure with
   | Closure (x, fBody, fDeclEnv, priv_TB) ->
-      let xVal, taintness = eval eArg env tClosure priv_TB inTrustBlock in
-        let fBodyEnv = (x, xVal, taintness) :: fDeclEnv in
+      let v, taintness = eval eArg env tClosure priv_TB inTrustBlock in
+        let fBodyEnv = (x, v, taintness) :: fDeclEnv in
           eval fBody fBodyEnv taintness priv_TB inTrustBlock
   | _ -> failwith "eval Call: not a function")
-| Trustblock tc ->
+| Trustblock content ->
     if inTrustBlock then failwith "You can't create a TrustBlock inside a TrustBlock"
     else
     if t then failwith "The content of the TrustBlock is tainted"
     else
-      eval tc env t priv_TB true
-| LetSecret (id, exprRight, letBody) ->
+      eval content env t priv_TB true
+| LetSecret (x, value, body) ->
     if not (inTrustBlock) then failwith "You have to be in a TrustBlock" 
     else
-    let xVal, taintness = eval exprRight env t priv_TB inTrustBlock in
-    let letEnv = bind_env env id xVal taintness in
-    let priv_TB2 = bind_tb priv_TB id Secret in
-    eval letBody letEnv t priv_TB2 inTrustBlock 
-| LetHandle (id, next) ->
+    let v, taintness = eval value env t priv_TB inTrustBlock in
+    let newEnv = bind_env env x v taintness in
+    let priv_TB2 = bind_tb priv_TB x Secret in
+    eval body newEnv t priv_TB2 inTrustBlock 
+| LetHandle (x, body) ->
   if not (inTrustBlock) then failwith "You have to be in a TrustBlock" 
   else
-  if secret_lookup priv_TB id then failwith "can't declare handle a secret"
-  else if pub_lookup priv_TB id then
-    let priv_TB2 = bind_tb priv_TB id Handler in
-    eval next env t priv_TB2 inTrustBlock
+  if secret_lookup priv_TB x then failwith "can't declare handle a secret"
+  else if pub_lookup priv_TB x then
+    let priv_TB2 = bind_tb priv_TB x Handler in
+    eval body env t priv_TB2 inTrustBlock
   else failwith "can't add to handle list a variable not trusted" 
-| LetPublic (id, exprRight, letBody) ->
+| LetPublic (x, value, body) ->
     if not (inTrustBlock) then failwith "You have to be in a TrustBlock" 
     else
-    let xVal, taintness = eval exprRight env t priv_TB inTrustBlock in
-    let letEnv = bind_env env id xVal taintness in
-    let priv_TB2 = bind_tb priv_TB id Public in
-    eval letBody letEnv t priv_TB2 inTrustBlock   
-| Include iBody -> (
+    let v, taintness = eval value env t priv_TB inTrustBlock in
+    let newEnv = bind_env env x v taintness in
+    let priv_TB2 = bind_tb priv_TB x Public in
+    eval body newEnv t priv_TB2 inTrustBlock   
+| Include content -> (
   if inTrustBlock then failwith "You can't include a file in a TrustBlock" 
   else
-  match iBody with
-  | Include _ -> failwith "Cannot nest include blocks"
+  match content with
+  | Include _ -> failwith "Cannot include within an Include"
   | Trustblock _ ->
       failwith "Cannot create TrustBlocks inside an Include"
-  | _ -> (ClosureInclude (iBody, env), t))
+  | _ -> (ClosureInclude (content, env), t))
 | Execute e -> 
   if inTrustBlock then failwith "You can't execute a file in a TrustBlock" 
   else
@@ -218,25 +205,22 @@ match e with
     | ClosureInclude (iBody, iEnv) -> eval iBody iEnv true priv_TB inTrustBlock
     | _ -> failwith "Unexpected condition."
   end
-| CallHandler (e1, e2) -> (
-    let v1, t1 = eval e1 env t priv_TB inTrustBlock in
+| CallHandler (trustblock, handler) -> (
+    let v1, t1 = eval trustblock env t priv_TB inTrustBlock in
       match (v1, t1) with
       | Secure_Block(priv_TB1, secondEnv), t1 -> 
-        if not (handle_lookup priv_TB1 e2) 
+        if not (handle_lookup priv_TB1 handler) 
           then failwith "You can access only an handle var" 
         else
-          (*let _ = print_list secondEnv in*)
-          eval e2 secondEnv t1 priv_TB1 inTrustBlock
+          eval handler secondEnv t1 priv_TB1 inTrustBlock
       | _ -> failwith "the access must be applied to an trustblock")
-| Assert e -> (
-      let v1, t1 = eval e env t priv_TB inTrustBlock in
+| Assert param -> (
+      let v1, t1 = eval param env t priv_TB inTrustBlock in
       if t1 = true then failwith "Assertion failed"
       else (Int 1,true)
       )
-| End ->  (
-          (*let _ = print_list priv_TB in*)
-          (Secure_Block(priv_TB,env),t) 
-          )
+| End ->  Secure_Block(priv_TB,env),t
+          
 
 (*----------------------------------------------------------------------------------------------------*)
 (*TYPE CHECKING*)
@@ -258,45 +242,53 @@ let rec type_check_com (c:exp) (c_env: conf_level c_env) (cxt: conf_level) : boo
                       let cxt1 = (join cxt t) in
                       (type_check_com c1 c_env cxt1) &&
                       (type_check_com c2 c_env cxt1)
-  | Let(x, e, c) -> 
+  | Let(x, value, body) -> 
                     let t = 
-                    match e with 
-                    | Trustblock _ -> let a = type_check_com e c_env cxt in
+                    match value with 
+                    | Trustblock _ -> let a = type_check_com value c_env cxt in
                                       if a = true 
                                         then Low
                                         else failwith "Information flow error in trustblock" 
-                    | _ -> type_check_exp e c_env 
+                    | _ -> type_check_exp value c_env 
                     in
                       let cxt1 = (join cxt t) in
                       if (lattice_checking cxt1 (Low)) then
                         let c_env1 = bind_cf c_env x Low in
-                          type_check_com c c_env1 cxt1
+                          type_check_com body c_env1 cxt1
                       else false
   | Fun(x, c) -> type_check_com c c_env cxt
-  | Trustblock c -> type_check_com c c_env cxt
-  | LetSecret(x, e, c) -> let t = type_check_exp e c_env in (*consideri la variabile definita x = HIGH *)
+  | Trustblock content -> type_check_com content c_env cxt
+  | LetSecret(x, value, body) -> let t = type_check_exp value c_env in (*consideri la variabile definita x = HIGH *)
                           let cxt1 = (join cxt t) in
                           if (lattice_checking cxt1 (High)) then
                             let c_env1 = bind_cf c_env x High in
-                              type_check_com c c_env1 cxt1
+                              type_check_com body c_env1 cxt1
                           else false
-  | LetPublic(x, e, c) -> let t = type_check_exp e c_env in (*consideri la variabile definita x = HIGH *)
+  | LetPublic(x, value, body) -> let t = type_check_exp value c_env in (*consideri la variabile definita x = HIGH *)
                           let cxt1 = (join cxt t) in
                           if (lattice_checking cxt1 (Low)) then
                             let c_env1 = bind_cf c_env x Low in
                               (*let _ = print_list c_env1 in*)
-                                type_check_com c c_env1 cxt1
+                                type_check_com body c_env1 cxt1
                           else false                        
-  | LetHandle(x, c) -> if (c_lookup c_env x) = High 
+  | LetHandle(x, body) -> if (c_lookup c_env x) = High 
                         then 
                             failwith "Handle must be Low"
                         else 
-                            type_check_com c c_env cxt
+                            type_check_com body c_env cxt
   | _-> true;;
 
 (*----------------------------------------------------------------------------------------------------*)
 (*TEST*)
 (*----------------------------------------------------------------------------------------------------*)
+
+
+ (*let rec print_list priv_TB =
+  (*Just to display on the terminal the evaluation result*)
+  match priv_TB with
+  | [] -> Printf.printf " Lista vuota \n"
+  | (x,s,_) :: rest -> let _ = Printf.printf " Ide %s, " x (*(string_of_conf_level s)*) in 
+                    print_list rest *)
 
 let print_eval (ris : evt * bool) =
   (*Just to display on the terminal the evaluation result*)
@@ -313,14 +305,14 @@ let print_type (ris : bool) =
   | true-> Printf.printf " Result: Well typed \n" 
   | false -> Printf.printf " Result: Illegal flow \n" ;;
 
-  let run_test name test_fn =   
-    try     
-      Printf.printf "Running %s...\n" name;     
-      test_fn ();     
-      Printf.printf "%s passed.\n" name;   
-    with   
-      | Failure msg -> Printf.printf "%s failed: %s\n" name msg   
-      | exn -> Printf.printf "%s failed with unexpected exception: %s\n" name (Printexc.to_string exn)
+let run_test name test_fn =   
+  try     
+    Printf.printf "Running %s...\n" name;     
+    test_fn ();     
+    Printf.printf "%s passed.\n" name;   
+  with   
+    | Failure msg -> Printf.printf "%s failed: %s\n" name msg   
+    | exn -> Printf.printf "%s failed with unexpected exception: %s\n" name (Printexc.to_string exn)
 
 (*TEST 1: try to innest a let inside onother let *)
 let test_1 () = 
